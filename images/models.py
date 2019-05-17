775f7ea0fc3344fbf0fddc6999eec23a08e21045
Ephraim-Kamau <kephraim19@gmail.com>
2019-05-17 15:29:05 +0300

    Add models to the database

--- images/models.py
+++ images/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+
+class Profile(models.Model):
+    bio = models.TextField()
+
+class Comments(models.Model):
+    comment = models.TextField()   
+
+class Image(models.Model):
+    name = models.CharField(max_length=30)
+    caption = models.TextField()
+    profile = models.ForeignKey(Profile)
+    comments = models.ForeignKey(Comments)
+
+
+ 
