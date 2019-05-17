3f6c48731d765221b9808a20c1a400a6fa35cdfe
Ephraim-Kamau <kephraim19@gmail.com>
2019-05-17 11:48:39 +0300

    Add templates

--- images/views.py
+++ images/views.py
@@ -3,7 +3,7 @@ import datetime as dt
 
 # Create your views here.
 def welcome(request):
-    return HttpResponse('Welcome to Instagram')
+    return render(request, 'welcome.html')
 
 def images_today(request):
     date = dt.date.today()
