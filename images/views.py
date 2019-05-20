0eaa6aa14b59d33cff31dcbb87d90aacfe755d9c
Ephraim-Kamau <kephraim19@gmail.com>
2019-05-20 17:09:44 +0300

    Add edit profile button

--- images/views.py
+++ images/views.py
@@ -29,4 +29,53 @@ def search_results(request):
 
     else:
         message = "You haven't searched for any term"
-        return render(request, 'all-images/search.html',{"message":message})    
+        return render(request, 'all-images/search.html',{"message":message})   
+
+def edit(request):
+    current_user_id=request.user.id
+    profile=Profile.objects.filter(userId=current_user_id)
+    if len(profile)<1:
+
+        if request.method=='POST':
+            form=EditProfile(request.POST,request.FILES)
+            if form.is_valid():
+                profile=form.save(commit=False)
+                profile.userId=current_user_id
+                profile.save()
+            return redirect("profile")
+        else:
+            form=EditProfile()
+            return render(request,"edit.html",{"form":form})
+    else:
+        if request.method=='POST':
+            form=EditProfile(request.POST,request.FILES )
+            if form.is_valid():
+                profile=form.save(commit=False)
+                bio=form.cleaned_data['bio']
+                pic=form.cleaned_data['pic']
+                update=Profile.objects.filter(userId=current_user_id).update(bio=bio,pic=pic)
+                profile.userId=current_user_id
+                profile.save(update)
+            return redirect("profile")
+        else:
+
+            form=EditProfile()
+
+            return render(request,"edit.html",{"form":form})   
+
+def uploads(request):
+    title='Upload'
+    current_user=request.user
+    current_user_id=request.user.id
+    if request.method=='POST':
+        form=PostImage(request.POST,request.FILES)
+        if form.is_valid():
+            image=form.save(commit=False)
+            image.user=current_user
+            image.userId=current_user_id
+            image.profile=current_user_id
+            image.save()
+        return redirect("profile")
+    else:
+        form=PostImage()
+    return render(request,"upload.html",{"title":title,"form":form})          
